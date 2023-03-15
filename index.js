f0c30a308e2a2f199b32b483c37ce809e551fd83
aalnoman10 <aalnomanm10@gmail.com>
2023-03-15 21:40:42 +0600

    update quiz question error

--- index.js
+++ index.js
@@ -58,16 +58,14 @@ const displayQuiz = (data) => {
     return;
   }
 
-  console.log(data);
   data.forEach((quiz, i) => {
-
-    quizContainer.innerHTML = quizContainer.innerHTML + `
+    quizContainer.innerHTML += `
       <div class="m-3 py-3 px-4 shadow-sm rounded">
         <div class="flex items-center">
           <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
             ${i + 1}
           </div>
-          <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+          <p class="text-gray-800 text-sm">${quiz.question}</p>
         </div>
         <div class="grid grid-cols-2 gap-4 mt-5">
           ${displayQuizOptions(quiz.options, i)}
