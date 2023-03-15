5176a5c5d561bde09c8f4d1a315b7f4adb7a98b2
aalnoman10 <aalnomanm10@gmail.com>
2023-03-15 21:33:03 +0600

    update quiz error

--- index.js
+++ index.js
@@ -60,17 +60,19 @@ const displayQuiz = (data) => {
 
   console.log(data);
   data.forEach((quiz, i) => {
-    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
-    </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
+
+    quizContainer.innerHTML = quizContainer.innerHTML + `
+      <div class="m-3 py-3 px-4 shadow-sm rounded">
+        <div class="flex items-center">
+          <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+            ${i + 1}
+          </div>
+          <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+        </div>
+        <div class="grid grid-cols-2 gap-4 mt-5">
+          ${displayQuizOptions(quiz.options, i)}
+        </div>
+      </div>`;
   });
 };
 
